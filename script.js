a7a6b65504e5966221a4a6b4e9892c241bcae0fd
mr.dingawy <165222355+Mr-dingawy@users.noreply.github.com>
2026-07-14 20:48:05 +0300

    working on variables and data-bs-theme

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const sideBtn = document.getElementById("side-btn");
 const side = document.getElementById("side");
 const sideClose = document.getElementById("side-btn-close");
+const btn = document.getElementById("themeToggle");
 
 
-console.log(side,sideBtn.sideClose)
-console.log(sideBtn)
-console.log(sideClose)
 
 sideBtn.addEventListener("click", () => {
     side.classList.toggle("hide");
 });
 sideClose.addEventListener("click", () => {
     side.classList.add("hide");
+});
+
+
+btn.addEventListener("click", () => {
+    const html = document.documentElement;
+    console.log(html)
+
+    if (html.getAttribute("data-bs-theme") === "dark") {
+        html.setAttribute("data-bs-theme", "light");
+        btn.classList.replace("bi-moon-fill", "bi-sun-fill");
+    } else {
+        html.setAttribute("data-bs-theme", "dark");
+        btn.classList.replace("bi-sun-fill", "bi-moon-fill");
+
+    }
 });
 
 
